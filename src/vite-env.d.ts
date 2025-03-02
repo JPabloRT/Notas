049aae2066866994eb631bee25b24c7efd8d8a71
JPabloRT <71268038+JPabloRT@users.noreply.github.com>
2025-03-02 12:37:03 -0600

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
-/// <reference types="vite/client" />
+// vite.config.ts
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/Notas/', // Esto es importante si tu repositorio no está en la raíz
+  plugins: []
+})
